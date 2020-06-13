487784e8776599ae264eda3ca7bd373ba756208f
Adrian <druids8012@gmail.com>
2020-06-13 22:22:48 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Button from './Button'
+import useColorSwitch from './useColorSwitch';
 
 function App() {
+  const [color,handleButton1Click] = useColorSwitch();
+  // const handleButton1Click = () => {
+  //   console.log("按钮1点击事件");
+  // }; 
+  const handleButton2Click = () => {
+    console.log("按钮2点击事件");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div> 
+    <Button  width="120px" label="按钮" onClicK={handleButton1Click} > 
+      <span>{1}</span>
+    </Button>
+    <p style={{color: color}}>这是一段文本</p>
+    <Button label="点我" onClicK={handleButton2Click} /> 
+    </div> 
   );
 }
 
---
+++ src/useColorSwitch.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+ 
+
+function useColorSwitch() {
+    const [color, setColor] = useState("#ff0000");
+
+    const handleButtonClick = () => {
+        setColor("#00ff00");
+    };
+    
+    return [color,handleButtonClick];
+}
+
+    export default useColorSwitch;
