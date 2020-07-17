73e8452b8a12a41990a3835be60e7f5611c6f97f
Adrian <druids8012@gmail.com>
2020-07-17 23:28:18 +0800

    input

--- src/theme.js
+++ src/theme.js
@@ -4,7 +4,11 @@ export default {
     gray: "rgba(24,28,28,0.2)",
     red : "#F34848",
     darkPurple:"#292F4c",
+    gray2: "rgba(241,237,237,0.3)",
+    gray3: "rgba(24, 28, 47, 0.3)" ,   
 
+    grayDark: "#1812F",
     normal: "1.4rem",
+    medium: "1.6rem",
 
 };
